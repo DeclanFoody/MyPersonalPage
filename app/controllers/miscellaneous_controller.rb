2a406ff571b21976f0b519b4eed48384e044f5e4
Fex Doody <fexdoody@Fexs-MacBook-Pro.local>
2016-09-23 18:05:27 -0700

    instagramapi-if clause

--- app/controllers/miscellaneous_controller.rb
+++ app/controllers/miscellaneous_controller.rb
@@ -1,8 +1,19 @@
 class MiscellaneousController < ApplicationController
+require('pry')
 helper_method :svg
-  def index
 
+def index
+    access_token = ""
+    client = Instagram.client(access_token: access_token)
+    @results = []
+    media = client.user_recent_media
+
+      media.each do |media, likes|
+
+        if media.likes["count"] >= 20
+          @results << media
+        end
+      end
+      return @results
   end
-
-  
 end
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+require 'instagram'
+Instagram.configure do |config|
+  config.client_id = ''
+  config.access_token = '' 
+end
